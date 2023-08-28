8d311bc7f1a7144b2308cd61979c61926fd9eb7a
Marino Bonetti <marinobonetti@gmail.com>
2023-08-28 16:56:05 +0200

    refactoring per leggibilità degli snapshot

--- spec/support/snapshot.rb
+++ spec/support/snapshot.rb
@@ -5,11 +5,16 @@ RSpec.configure do |config|
   config.snapshot_dir = "spec/fixtures/snapshots"
 
   config.after(:each, snapshot: true) do |example|
+    extension=nil
     extension = ".#{example.metadata[:snapshot]}" unless example.metadata[:snapshot] === true
     class_name = example.metadata[:described_class].name.underscore
     test_name = example.metadata[:full_description].gsub(example.metadata[:described_class].name, "").tr(" ", "_")
     raise "component snapshot has no content" if raw_rendered_content.blank?
-    expect(raw_rendered_content).to match_snapshot("#{class_name}/#{test_name}#{extension}")
+    str_content = raw_rendered_content
+    if extension == ".xml"
+      str_content = Nokogiri.XML(str_content).to_xml
+    end
+    expect(str_content).to match_snapshot("#{class_name}/#{test_name}#{extension}")
   end
 
 end
