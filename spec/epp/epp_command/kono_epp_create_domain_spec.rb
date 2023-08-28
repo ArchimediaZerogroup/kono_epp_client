62734a4756413af2701792417df098ce3926d16b
Marino Bonetti <marinobonetti@gmail.com>
2023-08-28 16:18:47 +0200

    la creazione dei dominio può avvenire senza avere l'ip del ns

---
+++ spec/epp/epp_command/kono_epp_create_domain_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+RSpec.describe KonoEppCreateDomain do
+  include_context "like epp command"
+
+  let(:instance) {
+    KonoEppCreateDomain.new(
+      {name: 'architest.it',
+       period: 1,
+       nameservers:
+         [["ns.test.it", "192.168.100.10"],
+          ["ns2.test.it", "192.168.100.20"],
+          ["ns3.foo.com"]],
+       # contacts
+       registrant: "RRR12",
+       admin: "AAAA12",
+       tech: "TTT12",
+       authinfo: "WWW-test-it"
+      }
+    )
+  }
+
+  it "create", snapshot: "xml" do
+    expect(rendered.to_s).to have_tag("ns")
+    expect(rendered.to_s.downcase).to have_tag("hostattr")
+    expect(rendered.to_s.downcase).to have_tag("ns hostattr" ) do
+      with_tag("hostname",text:"ns2.test.it")
+      with_tag("hostaddr",text:"192.168.100.20",with:{ip:"v4"})
+    end
+    expect(rendered.to_s.downcase).to have_tag("ns hostattr" ) do
+      with_tag("hostname",text:"ns.test.it")
+      with_tag("hostaddr",text:"192.168.100.10",with:{ip:"v4"})
+    end
+    expect(rendered.to_s.downcase).not_to have_tag("ns hostaddr", with: {ip: 'v4'},text:"" )
+  end
+
+end
