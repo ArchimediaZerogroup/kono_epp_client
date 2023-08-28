6e3da025d247423356c2e4a4ae260654ae163b3b
Marino Bonetti <marinobonetti@gmail.com>
2023-08-28 17:29:46 +0200

    refactoring codice per essere logico nelle varie operazioni

--- lib/epp/epp_command/update_domain.rb
+++ lib/epp/epp_command/update_domain.rb
@@ -12,67 +12,67 @@ class KonoEppUpdateDomain < KonoEppCommand
     name.text = options[:name]
 
     if not options[:add_nameservers].blank? or options[:add_admin] or options[:add_tech] or options[:add_status]
+      # <domain:add>
       domain_add = domain_update.add_element "domain:add"
+
+      unless options[:add_nameservers].blank?
+        domain_add_ns = domain_add.add_element "domain:ns"
+
+        options[:add_nameservers].each do |ns|
+          host_attr = domain_add_ns.add_element "domain:hostAttr"
+          host_name = host_attr.add_element "domain:hostName"
+
+          host_name.text = ns[0]
+
+          # FIXME IPv6
+          host_addr = host_attr.add_element "domain:hostAddr", {"ip" => "v4"}
+          host_addr.text = ns[1]
+        end
+      end
+
+      if options[:add_admin]
+        domain_contact = domain_add.add_element "domain:contact", {"type" => "admin"}
+        domain_contact.text = options[:add_admin]
+      end
+
+      if options[:add_status]
+        domain_add.add_element "domain:status", {"s" => options[:add_status]}
+      end
+
+      if options[:add_tech]
+        domain_contact = domain_add.add_element "domain:contact", {"type" => "tech"}
+        domain_contact.text = options[:add_tech]
+      end
     end
 
     if not options[:remove_nameservers].blank? or options[:remove_admin] or options[:remove_tech] or options[:remove_status]
+      # <domain:rem>
       domain_remove = domain_update.add_element "domain:rem"
-    end
 
-    # <domain:add>
-    unless options[:add_nameservers].blank?
-      domain_add_ns = domain_add.add_element "domain:ns"
+      unless options[:remove_nameservers].blank?
+        domain_remove_ns = domain_remove.add_element "domain:ns"
 
-      options[:add_nameservers].each do |ns|
-        host_attr = domain_add_ns.add_element "domain:hostAttr"
-        host_name = host_attr.add_element "domain:hostName"
+        options[:remove_nameservers].each do |name|
+          host_attr = domain_remove_ns.add_element "domain:hostAttr"
+          host_name = host_attr.add_element "domain:hostName"
 
-        host_name.text = ns[0]
-
-        # FIXME IPv6
-        host_addr = host_attr.add_element "domain:hostAddr", {"ip" => "v4"}
-        host_addr.text = ns[1]
+          host_name.text = name
+        end
       end
-    end
 
-    if options[:add_admin]
-      domain_contact = domain_add.add_element "domain:contact", {"type" => "admin"}
-      domain_contact.text = options[:add_admin]
-    end
-
-    if options[:add_status]
-       domain_add.add_element "domain:status", {"s" => options[:add_status]}
-    end
-
-    if options[:add_tech]
-      domain_contact = domain_add.add_element "domain:contact", {"type" => "tech"}
-      domain_contact.text = options[:add_tech]
-    end
-
-    # <domain:rem>
-    unless options[:remove_nameservers].blank?
-      domain_remove_ns = domain_remove.add_element "domain:ns"
-
-      options[:remove_nameservers].each do |name|
-        host_attr = domain_remove_ns.add_element "domain:hostAttr"
-        host_name = host_attr.add_element "domain:hostName"
-
-        host_name.text = name
+      if options[:remove_admin]
+        domain_contact = domain_remove.add_element "domain:contact", {"type" => "admin"}
+        domain_contact.text = options[:remove_admin]
       end
-    end
 
-    if options[:remove_admin]
-      domain_contact = domain_remove.add_element "domain:contact", {"type" => "admin"}
-      domain_contact.text = options[:remove_admin]
-    end
+      if options[:remove_status]
+        domain_remove.add_element "domain:status", {"s" => options[:remove_status]}
+      end
 
-    if options[:remove_status]
-      domain_remove.add_element "domain:status", {"s" => options[:remove_status]}
-    end
-
-    if options[:remove_tech]
-      domain_contact = domain_remove.add_element "domain:contact", {"type" => "tech"}
-      domain_contact.text = options[:remove_tech]
+      if options[:remove_tech]
+        domain_contact = domain_remove.add_element "domain:contact", {"type" => "tech"}
+        domain_contact.text = options[:remove_tech]
+      end
     end
 
     # <domain:chg>
