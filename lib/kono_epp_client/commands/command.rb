3bde7194f2486d2bf6116a55e1268e40033d5a2d
Marino Bonetti <marinobonetti@gmail.com>
2025-03-12 16:38:14 +0100

    fix: Correct Constants require

--- lib/kono_epp_client/commands/command.rb
+++ lib/kono_epp_client/commands/command.rb
@@ -1,10 +1,9 @@
-include REXML
 module KonoEppClient::Commands
-  class Command < REXML::Document
+  class Command < ::REXML::Document
     def initialize(source = nil, context = nil)
       super(source, context)
 
-      add(XMLDecl.new("1.0", "UTF-8", "no"))
+      add(::REXML::XMLDecl.new("1.0", "UTF-8", "no"))
 
       epp = add_element("epp", {"xmlns" => "urn:ietf:params:xml:ns:epp-1.0",
                                 "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
--- lib/kono_epp_client/commands/hello.rb
+++ lib/kono_epp_client/commands/hello.rb
@@ -1,9 +1,9 @@
 module KonoEppClient::Commands
-  class Hello < REXML::Document
+  class Hello < ::REXML::Document
     def initialize
       super
 
-      add(XMLDecl.new("1.0", "UTF-8", "no"))
+      add(::REXML::XMLDecl.new("1.0", "UTF-8", "no"))
 
       epp = add_element("epp", {"xmlns" => "urn:ietf:params:xml:ns:epp-1.0",
                                 "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
