83fa9a51083ad2ad14fa7fa02ab5f8589727c151
Marino Bonetti <marinobonetti@gmail.com>
2025-03-13 10:16:40 +0100

    test: Fix correct name generation

--- spec/support/snapshot.rb
+++ spec/support/snapshot.rb
@@ -1,5 +1,14 @@
 require "rspec/snapshot"
 require "active_support/core_ext/string"
+
+
+def first_parent_example_group(metadata)
+  parent = metadata[:parent_example_group] || metadata[:example_group]
+  return metadata if parent.nil?
+  first_parent_example_group(parent)
+end
+
+
 RSpec.configure do |config|
 
   config.snapshot_dir = "spec/fixtures/snapshots"
@@ -7,8 +16,9 @@ RSpec.configure do |config|
   config.after(:each, snapshot: true) do |example|
     extension=nil
     extension = ".#{example.metadata[:snapshot]}" unless example.metadata[:snapshot] === true
-    class_name = example.metadata[:described_class].name.underscore
-    test_name = example.metadata[:full_description].gsub(example.metadata[:described_class].name, "").tr(" ", "_")
+    described_class = first_parent_example_group(example.metadata)[:described_class]
+    class_name = described_class.name.underscore
+    test_name = example.metadata[:full_description].gsub(described_class.name, "").tr(" ", "_")
     raise "component snapshot has no content" if raw_rendered_content.nil? or raw_rendered_content.empty?
     str_content = raw_rendered_content
     if extension == ".xml"
