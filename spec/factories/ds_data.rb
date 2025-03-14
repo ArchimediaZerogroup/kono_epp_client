5dd6d7eb8ce78bf8586112e0908436387af24f4f
Marino Bonetti <marinobonetti@gmail.com>
2025-03-14 09:10:20 +0100

    test: Digest con un dato realistico

--- spec/factories/ds_data.rb
+++ spec/factories/ds_data.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     key_tag { 123 }
     alg { :dsa_sha_1 }
     digest_type { :sha_1 }
-    digest { "digest" }
+    digest { "4347d0f8ba661234a8eadc005e2e1d1b646c9682" }
 
     initialize_with { new(key_tag, alg, digest_type, digest) }
   end
