4c246172f3b8de95f1bdfb862c090c87190c1376
Marino Bonetti <marinobonetti@gmail.com>
2025-03-14 09:51:33 +0100

    feat: Update Domain command

--- lib/kono_epp_client/commands/update_domain.rb
+++ lib/kono_epp_client/commands/update_domain.rb
@@ -101,6 +101,17 @@ module KonoEppClient::Commands
         end
       end
       # TODO: Registrant
+
+      if options[:dns_sec_data] and options[:dns_sec_data].any?
+        extension = command.elements['extension'] || command.add_element("extension")
+
+        create_list = extension.add_element("secDNS:update", {"xmlns:secDNS"=> "urn:ietf:params:xml:ns:secDNS-1.1"})
+
+        options[:dns_sec_data].each do |d|
+          create_list.add_element(d)
+        end
+      end
+
     end
   end
 end
